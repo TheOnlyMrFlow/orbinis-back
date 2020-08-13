6789aac82a353df2bfc9398f2a442eef5688b30c
MrFLow <comte.florian@gmail.com>
2020-08-13 13:26:46 +0200

    fixed process.env call

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,10 +5,10 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'pg',
-        host: "${process.env.DATABASE_HOST}",
-        user: "${process.env.DATABASE_USER}",
-        password: "${process.env.DATABASE_PASSWORD}",
-        database: "${process.env.DATABASE_NAME}",
+        host: `${process.env.DATABASE_HOST}`,
+        user: `${process.env.DATABASE_USER}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        database: `${process.env.DATABASE_NAME}`,
         charset: 'utf8',
         ssl: { "rejectUnauthorized": false }
       },
