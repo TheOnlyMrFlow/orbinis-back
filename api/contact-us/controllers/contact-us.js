fd0ed9501873b271ae30c05ab35700442b0b6cfd
MrFLow <comte.florian@gmail.com>
2020-09-23 22:09:04 +0200

    contact us sends feedback to sender and notify staff

--- api/contact-us/controllers/contact-us.js
+++ api/contact-us/controllers/contact-us.js
@@ -7,21 +7,40 @@
 module.exports = {
   post: async (ctx, next) => {
     try {
-      ctx.body = 'ok';
-      console.log('Before mail');
-      await strapi.plugins['email'].services.email.send({
-        to: 'comte.florian@gmail.com',
-        from: 'comte.florian@gmail.com',
-        //cc: 'helenedarroze@strapi.io',
-        //bcc: 'ghislainearabian@strapi.io',
-        replyTo: 'comte.florian@gmail.com',
-        subject: 'Email working',
-        text: 'Hello world!',
-        html: 'Hello world!',
-      });
-      console.log('After mail');
+      const reqBody = ctx.request.body;
+      await sendFeedbackToCustomer(reqBody.customerName, reqBody.customerAddress,reqBody.customerMessage)
+      await notifyOrbinisStaff(reqBody.customerName, reqBody.customerAddress,reqBody.customerMessage)
     } catch (err) {
       ctx.body = err;
+      ctx.status = 500;
     }
   }
 };
+
+async function sendFeedbackToCustomer(customerName, customerAddress, customerMessage) {
+    return strapi.plugins['email'].services.email.send({
+      to: customerAddress,
+      from: process.env.MAIL_SENDER_ADDRESS,
+      replyTo: process.env.MAIL_SENDER_ADDRESS,
+      subject: 'We received your contact request !',
+      html: `
+      <p>Hey ${customerName} ! We received your contact request and will get back to you soon !</p>
+      <p>Your message was : </p>
+      <p><em>${customerMessage}</em></p>
+      `
+    });
+}
+
+async function notifyOrbinisStaff(customerName, customerAddress, customerMessage) {
+  return strapi.plugins['email'].services.email.send({
+    to: process.env.MAIL_SENDER_ADDRESS,
+    from: process.env.MAIL_NOTIFIER_ADDRESS,
+    replyTo: process.env.MAIL_NOTIFIER_ADDRESS,
+    subject: `${customerName} a envoyé une demande de contact`,
+    html: `
+    <p>${customerName} a envoyé une demande de contact avec le message suivant : </p>
+    <p><em>${customerMessage}</em></p>
+    <p>Son adresse est : ${customerAddress}</p>
+    `
+  });
+}
