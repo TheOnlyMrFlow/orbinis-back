475c46c3cbe123b0a8d42d406d778f880771ebb6
TheOnlyMrFlow <comte.florian@gmail.com>
2022-02-09 18:30:03 +0100

    added new fields

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,15 +1,36 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('MONGO_HOST'),
+        srv: true,
+        port: 27017,
+        database: 'strapi',
+        username: env('MONGO_USER'),
+        password: env('MONGO_PASSWORD'),
       },
       options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+        authenticationDatabase: 'admin',
+        ssl: true
+      }
+    }
+  }
 });
